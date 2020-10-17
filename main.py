f46d54f9b81ac429e3a65a85d82f3884ce9200f9
jhonathanmusa <jonathansm891@gmail.com>
2020-10-16 20:37:35 -0500

    thirth commit

--- main.py
+++ main.py
@@ -7,10 +7,22 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
+# Services 
 @app.route("/services")
 def services():
     return render_template('services.html')
+
+@app.route("/service_one")
+def service_one():
+    return render_template("service-one.html")
+
+@app.route("/service_two")
+def service_two():
+    return render_template("service-two.html")
+
+@app.route("/service_three")
+def service_three():
+    return render_template("service-three.html")
 
 
 @app.route("/products")
